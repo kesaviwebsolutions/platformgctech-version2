5177aaa018c14e9f744507a42446bc5526695aff
Nikhil Kharana <60419615+Nikkrana28@users.noreply.github.com>
2022-09-12 07:53:17 +0530

    update the level

--- src/compnents/pages/CreateLevel.tsx
+++ src/compnents/pages/CreateLevel.tsx
@@ -1,39 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MDBInput } from "mdb-react-ui-kit";
 import { Box, Container } from "@mui/system";
 import { Button, Typography } from "@mui/material";
 import AdminNav from "../AdminNav";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+const url = "https://refer.ap.ngrok.io";
+// const url = "http://localhost:3030";
+
+const notify = (msg) => toast.success(msg);
 
 export default function CreateLevel() {
+
+  const [name, setName] = useState(0)
+  const [bonus, setBonus] = useState(0)
+  const [reward, setReward] = useState(0)
+  const [ref, setRef] = useState(0);
+
+  useEffect(()=>{
+    
+
+   
+  },[])
+
+  const CreateLevel =async()=>{
+    axios.post(`${url}/levelcreate`,{
+      Name:name,
+      Bonus:bonus,
+      Reward:reward,
+      NoRefReq:ref
+    }).then((res)=>{
+      console.log(res)
+      notify("Successfull level created")
+    })
+  }
+
   return (
     <Container maxWidth="lg">
       <AdminNav />
       <Box style={{ margin: "0px 0px 50px" }}>
         <Box style={{ margin: "70px 0px 0px" }}>
           <Typography paragraph>Name of the level</Typography>
-          <MDBInput label="Ex. Level 1" id="form1" type="text" />
+          <MDBInput label="Ex. Level 1" id="form1" type="Number" onChange={(e)=>setName(e.target.value)}/>
         </Box>
         <Box>
           <Typography paragraph style={{ marginTop: "20px" }}>
             Bonus(%)
           </Typography>
-          <MDBInput label="2%" id="form1" type="text" />
+          <MDBInput label="2%" id="form1" type="Number" onChange={(e)=>setBonus(e.target.value)}/>
         </Box>
         <Box>
           <Typography paragraph style={{ marginTop: "20px" }}>
             Reward
           </Typography>
-          <MDBInput label="2%%" id="form1" type="text" />
+          <MDBInput label="2%%" id="form1" type="Number" onChange={(e)=>setReward(e.target.value)}/>
         </Box>
         <Box>
           {" "}
           <Typography paragraph style={{ marginTop: "20px" }}>
             NO. of referral required to quality
           </Typography>
-          <MDBInput label="10" id="form1" type="text" />
+          <MDBInput label="10" id="form1" type="Number" onChange={(e)=>setRef(e.target.value)}/>
         </Box>
-        <Button className="createbutton">Create</Button>
+        <Button className="createbutton" onClick={()=>CreateLevel()}>Create</Button>
       </Box>
+      <Toaster/>
     </Container>
   );
 }
